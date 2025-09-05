a713a48af90e39fed54e66a079b4d15d841a00a6
Shubham Dwivedi <51696142+shubh-dwivedi@users.noreply.github.com>
2025-09-05 20:49:39 +0530

    minor changes

--- functions/app.js
+++ functions/app.js
@@ -1,11 +1,11 @@
 const fetch = (...args) =>
-  import('node-fetch').then(({ default: fetch }) => fetch(...args));
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
 const express = require("express");
 const serverless = require("serverless-http");
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
-var cors = require('cors')
+var cors = require("cors");
 const app = express();
 const router = express.Router();
 
@@ -14,31 +14,53 @@ const apiKey2 = process.env.API_KEY_BACKUP;
 const apiUrl = process.env.NEWS_API_URL;
 
 router.get("/", (req, res) => {
-    res.send("App is running..");
+  res.send("App is running..");
 });
 
-router.get('/translate/fetchnews', async (req, res) => {
+router.get("/translate/fetchnews", async (req, res) => {
   try {
     let url = apiUrl;
-    url += req.query.country ? '&country=' + req.query.country : '';
-    url += req.query.category ? '&category=' + req.query.category : '';
-    url += "&apiKey=" + apiKey + '&page=' + req.query.page + '&pageSize=' + req.query.pageSize;
+    if (req.query.q) url += "q=" + req.query.q;
+    if (req.query.country) url += "&country=" + req.query.country;
+    if (req.query.category) url += "&category=" + req.query.category;
+    url += `&apiKey=${apiKey}&page=${req.query.page}&pageSize=${req.query.pageSize}`;
 
     const response = await fetch(url);
     const data = await response.json();
-    const errorCode = data.code || '';
-    if(data.status === 'ok') {
-      res.json(data)
-    } else if(errorCode === "apiKeyExhausted" || errorCode === "rateLimited") {
-      url.replace(apiKey, apiKey2)
+    const statusCode = data.code || "";
+    if (data.status === "ok") {
+      res.json(data);
+    } else if (
+      statusCode === "apiKeyExhausted" ||
+      statusCode === "rateLimited"
+    ) {
+      url.replace(apiKey, apiKey2);
       const response2 = await fetch(url);
       const data2 = await response2.json();
-      res.json(data2)
+      res.json({
+        ...data2,
+        info: {
+          search: req.query.q || null,
+          country: req.query.country || null,
+          category: req.query.category || null,
+          page: req.query.page || null,
+          pageSize: req.query.pageSize || null,
+        },
+      });
     } else {
-      res.json(data)
+      res.json({
+        info: {
+          ...data,
+          search: req.query.q || null,
+          country: req.query.country || null,
+          category: req.query.category || null,
+          page: req.query.page || null,
+          pageSize: req.query.pageSize || null,
+        },
+      });
     }
   } catch (error) {
-    console.log(error);
+    // console.log(error);
     res.status(500).send("Internal Server Error");
   }
 });
